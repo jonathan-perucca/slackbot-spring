212c3437c736572232c051ecee86fd31b8b632fa
jperucca <under.64@gmail.com>
2016-07-04 00:28:29 +0200

    slackbot app > remove startup logs

--- src/main/java/com/github/slackbot/SlackBotApplication.java
+++ src/main/java/com/github/slackbot/SlackBotApplication.java
@@ -1,35 +1,10 @@
 package com.github.slackbot;
 
-import com.github.slackbot.core.SlackClient;
-import com.ullink.slack.simpleslackapi.*;
-import org.slf4j.Logger;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 @SpringBootApplication
 public class SlackBotApplication {
-
-	private static final Logger logger = getLogger(SlackBotApplication.class);
-
-	@Autowired
-	SlackClient client;
-
-	@Bean
-	public InitializingBean startup() {
-		return () -> {
-			SlackSession session = client.getSession();
-
-			session.getUsers()
-				   .stream()
-				   .forEach(slackUser -> logger.info(slackUser.getRealName()));
-		};
-	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(SlackBotApplication.class, args);
